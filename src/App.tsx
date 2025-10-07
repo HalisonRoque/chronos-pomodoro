733156f588d7eccf61f78ccbe4a17bed42421b32
halisonyasha@gmail.com <halisonyasha@gmail.com>
2025-10-07 18:44:51 -0300

    novo projeto React com Vite e Nest.js

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './styles/theme.css'
+import './styles/global.css'
+import { Heading } from './components/Heading'
+
+export function App() {
+
+  return (
+    <>
+      <div>
+        <Heading />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed numquam nihil, laboriosam in, quasi voluptates maxime vero provident nostrum tempora laborum quas, minus natus praesentium quaerat dolor pariatur suscipit veniam!</p>
+      </div>
+    </>
+  )
+}
+
