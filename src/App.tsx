60b847b04f01c7c58961ca103e14afd65875fbd9
halisonyasha@gmail.com <halisonyasha@gmail.com>
2025-10-07 20:18:08 -0300

    novo commit

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ export function App() {
   return (
     <>
       <div>
-        <Heading />
+        <Heading>Olá mundo props</Heading>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed numquam nihil, laboriosam in, quasi voluptates maxime vero provident nostrum tempora laborum quas, minus natus praesentium quaerat dolor pariatur suscipit veniam!</p>
       </div>
     </>
