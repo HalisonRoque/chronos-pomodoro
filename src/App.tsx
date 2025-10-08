1971828e87eeb2dddb54611fe636b0601bd40e03
halisonyasha@gmail.com <halisonyasha@gmail.com>
2025-10-08 16:49:56 -0300

    criação de estrutura html do site

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
 import './styles/theme.css'
 import './styles/global.css'
-import { Heading } from './components/Heading'
 
 export function App() {
 
   return (
     <>
-      <div>
-        <Heading>Olá mundo props</Heading>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed numquam nihil, laboriosam in, quasi voluptates maxime vero provident nostrum tempora laborum quas, minus natus praesentium quaerat dolor pariatur suscipit veniam!</p>
+      <div className='container'>
+        <div className='content'>
+          <section>LOGO</section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>MENU</section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>FORM</section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>FOOTER</section>
+        </div>
       </div>
     </>
   )
